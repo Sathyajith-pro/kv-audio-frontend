3770b858ea67ff04f7702e23d1566ac868b11810
Anuththara Sathyajith <wickramage28@gmail.com>
2025-01-22 11:25:45 +0530

    installl tailwind css😊

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -4,11 +4,7 @@ export default function ProductCard(props){
 
     return(
         <div>
-            <img src ={props.photoUrl}></img>
-            <span>J{props.name}</span>
-            <span>LKR. {props.price}</span>
-            <p>{props.description}</p>
-              
+            
         </div>
     )
 }
