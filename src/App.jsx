49c41ace1a7e12f25b4574ea96f3b8980de368d1
Anuththara Sathyajith <wickramage28@gmail.com>
2025-01-22 13:16:48 +0530

    apply tailwind css & use react icons 😊🦟

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
+import { MdOutlineSpeaker } from 'react-icons/md';
 import './App.css'
 import ProductCard from './components/productCard'
+import { BsGraphDown } from "react-icons/bs";
+import { FaRegBookmark, FaRegUser } from "react-icons/fa";
 
 function App() {
  
 
   return (
-   <div className=''>
-    asad
+  <div className='w-full h-screen flex' >
+
+    <div className='w-[400px] h-full bg-green-200'>
+
+      <button className='w-full h-[40px] text-[25px] font-bold
+        flex justify-center items-center'>
+        <BsGraphDown/>Dashboard 
+        </button>
+      <button className='w-full h-[40px] text-[25px] font-bold
+      flex justify-center items-center'>
+        <FaRegBookmark/>
+         Booking
+       </button>
+      <button className='w-full h-[40px] text-[25px] font-bold
+       flex justify-center items-center'> 
+       <MdOutlineSpeaker/>
+        Items 
+        </button>
+      <button className='w-full h-[40px] text-[25px] font-bold
+      flex justify-center items-center'>
+        <FaRegUser/>
+         Users 
+        </button>
     </div>
+      <div className='w-full bg-red-900'>
+        
+
+      </div>
+    
+    
+</div>
 )
 }
 export default App
