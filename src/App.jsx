7dd80f141cd4499962a55db9da4896e923da93a2
Anuththara Sathyajith <wickramage28@gmail.com>
2025-01-31 18:11:34 +0530

    create login page & use hooks 🎈😊

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import AdminPage from './pages/admin/adminPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home/homePage';
+import LoginPage from './pages/login/login';
 
 function App() {
  
@@ -13,6 +14,7 @@ function App() {
     
     <Route path = "/admin/*" element = {<AdminPage/>}/>
     <Route path = "/*" element = {<HomePage/>}/>
+    <Route path = "/login" element = {<LoginPage/>}/>
     
 
   </Routes>
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import "./login.css";
+
+export default function LoginPage(){
+
+    const [email,setEmail] = useState("");
+    const [password,setPassword]=useState("");
+
+
+    return(
+        
+        <div className="w-full h-screen bg-picture flex items-center justify-center">
+            <form>
+            <div className="w-[400px] h-[400px] backdrop-blur-xl rounded-2xl flex justify-center items-center flex-col relative">
+                <img src="/logo.png" className="w-[110px] h-[110px] absolute top-1 object-cover"/>
+
+                <input type= "email"  placeholder="Email" className="  mt-6 w-[300px] h-30px] bg-transparent border-b-2 border-white text-white text-xl outline-none "value={email} onChange={(e)=>{
+                    setEmail(e.target.value)
+                }}/>
+                
+                <input type= "password" placeholder="Password" className="mt-6 w-[300px] h-[30px] bg-transparent border-b-2 border-white text-white text-xl outline-none "value={password} onChange={(e)=>{
+                   setPassword(e.target.value)
+                }}/>
+
+                <button className="w-[300px] h-[50px] bg-[#efac38] text-2xl text-white rounded-lg my-8" >Login</button>
+
+            </div>
+            </form>
+        </div>
+    )
+}
