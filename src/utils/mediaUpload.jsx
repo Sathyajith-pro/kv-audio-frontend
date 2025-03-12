c695c893b699811177cdc1228bb435cc9c866c5b
Anuththara Sathyajith <wickramage28@gmail.com>
2025-03-12 09:37:34 +0530

    netlify json updated 🤙😃

--- src/utils/mediaUpload.jsx
+++ src/utils/mediaUpload.jsx
@@ -1,7 +1,7 @@
  import { createClient } from "@supabase/supabase-js"
 
- const anon_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlodXhnaWFvbXBybWdvdmR3dnp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1NTAzNjQsImV4cCI6MjA1NjEyNjM2NH0.QXlsAsLzXUtvvqqWYKnPE63q_F-b_DwmTLnW1GyDclA"
- const supabase_url ="https://yhuxgiaomprmgovdwvzw.supabase.co"
+ const anon_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhbGd6Z2llZ3lqZ2pnaHF1ZW5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExNTQxNTksImV4cCI6MjA1NjczMDE1OX0.3GrO7jsIyu-9Q3Ft0rdyh9SPNqXrickUUuK8NEh3Vqc"
+ const supabase_url ="https://ualgzgiegyjgjghquenc.supabase.co"
 
  const supabase = createClient (supabase_url,anon_key)
 
@@ -15,11 +15,11 @@
         const timestamp = new Date().getTime();
     const fileName = timestamp+file.name
 
-     supabase.storage.from("images").upload(fileName,file,{
+     supabase.storage.from("image").upload(fileName,file,{
         cacheControl: `3600`,
         upsert: false,
      }).then(()=>{
-         const publicUrl = supabase.storage.from("images").getPublicUrl(file.name).data.publicUrl;
+         const publicUrl = supabase.storage.from("image").getPublicUrl(fileName).data.publicUrl;
          resolve(publicUrl)
      }).catch(()=>{
         reject("Error uploading file")
